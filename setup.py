ea85a37516e609aa555e5d263684ccac1e0b323c
x094932 <x094932@PR07140X094932U.ad.produbanes.corp>
2016-01-05 16:12:54 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup (
+       name='dadgraphics',
+       version='0.1',
+       packages=find_packages(),
+
+       # Declare your packages' dependencies here, for eg:
+       install_requires=['PySDL2>=0.9.3'],
+
+       # Fill in these to make your Egg ready for upload to
+       # PyPI
+       author='idaho06',
+       author_email='',
+
+       #summary = 'Just another Python package for the cheese shop',
+       url='',
+       license='',
+       long_description='Easy graphics library so my dad es able to program in python',
+
+       # could also include long_description, download_url, classifiers, etc.
+
+  
+       )
